848d95fe885211d41f36c5a8c10c4adcedc3634a
Matteo Brunati <1902043+mattbrun@users.noreply.github.com>
2023-09-08 02:05:49 +0200

    Update install.sh
    - Highlight messages intentions (i.e. information vs warning)
    - Backup existing config if any
    - Minor layout graphical change

--- install.sh
+++ install.sh
@@ -1,15 +1,27 @@
 #!/usr/bin/env bash
+
 current_dir=$(dirname "$0")
-config_dir=$HOME/.config/terminator/
-echo "This script will install dracula color theme to Terminator."
-echo "This will replace the current terminator config file"
+config_dir="$HOME/.config/terminator"
+
+echo "[II] This script will install dracula color theme to Terminator."
+echo "[WW] This will replace the current terminator config file after backing it up"
+echo "[WW] In order to work dracula theme needs Anonymous Pro font to work"
+
 function copy_config() {
-    echo "Applying Dracula theme..."
-    echo "Copying config file to ~/.config/terminator"
-    mkdir -p $config_dir
-    cp -rf  $current_dir/config/config $config_dir
-    echo "terminator config updated!"
-    echo "If you want to edit config, edit ~/.config/terminator/config file"
+    echo "[II] Applying Dracula theme..."
+
+    if [ -f $config_dir/config ]; then
+      echo "[II] Backing up old terminator config to $config_dir/config.bkp"
+      cp $config_dir/config $config_dir/config.bkp
+    else
+      mkdir -p $config_dir
+    fi
+
+    echo "[II] Copying config file to ~/.config/terminator"
+    cp -rf  $current_dir/config/config $config_dir/
+
+    echo "[II] terminator config updated!"
+    echo "[II] If you want to edit config, edit $config_dir/config file"
 }
 
 while true; do
@@ -22,4 +34,3 @@ read -p "Do you wish to continue? " yn
         * ) echo "Please answer yes or no.";;
     esac
 done
-
